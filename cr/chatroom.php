635a4212db7dbb62d6a1ecf01ef43e40cb8c0017
Joe <167563398+Joercat@users.noreply.github.com>
2024-09-04 12:29:08 -0400

    Update chatroom.php

--- cr/chatroom.php
+++ cr/chatroom.php
@@ -1,22 +1,10 @@
 <?php
 session_start();
 
-// Dummy user data (in a real scenario, this would be fetched from a database)
-$valid_users = array(
-    'jr' => 'pass',
-    'user2' => 'password2'
-);
-
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $username = $_POST['username'];
-    $password = $_POST['password'];
-
-    if (isset($valid_users[$username]) && $valid_users[$username] === $password) {
-        $_SESSION['username'] = $username;
-        header('Location: chatroom.html');
-        exit;
-    } else {
-        echo '<script>alert("Invalid username or password");</script>';
-    }
+    $_SESSION['username'] = $username;
+    header('Location: chatroom.html');
+    exit;
 }
 ?>
