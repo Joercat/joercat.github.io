a4a545dd9469f20784e8ff6563601e3448bdb527
Joe <167563398+Joercat@users.noreply.github.com>
2024-09-24 10:16:28 -0400

    Create greet.js

---
+++ index/greet.js
@@ -0,0 +1,16 @@
+   document.getElementById("a");
+       var today = new Date();
+var hourNow = today.getHours();
+var greeting;
+
+if (hourNow > 18) {
+    greeting = 'Good evening!';
+} else if (hourNow > 12) {
+    greeting = 'Good afternoon!';
+} else if (hourNow > 0) {
+    greeting = 'Good morning!';
+} else {
+    greeting = 'Hello!';
+}
+
+document.write('<h1 class="welcome">' + greeting + '</h1>');
